942bf02e0a7feb70e3852cb0602b6693e3199037
Ganesh V Bidve <31819293+GaneshBidve@users.noreply.github.com>
2020-02-16 19:55:06 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -118,4 +118,105 @@ Pattern-4
 			System.out.println();
 		}
 	}
+	
+		
+	/*
+	 * Pattern-5
+--------------
+9 8 7 6 5 4 3 2 1 0 
+9 8 7 6 5 4 3 2 1 0 
+9 8 7 6 5 4 3 2 1 0 
+9 8 7 6 5 4 3 2 1 0 
+9 8 7 6 5 4 3 2 1 0 
+9 8 7 6 5 4 3 2 1 0 
+9 8 7 6 5 4 3 2 1 0 
+9 8 7 6 5 4 3 2 1 0 
+9 8 7 6 5 4 3 2 1 0 
+9 8 7 6 5 4 3 2 1 0 
+	 */
+	public static void pattern5() {
+		System.out.println("Pattern-5");
+		System.out.println("--------------");
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.print((9 - j) + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	/*
+	 * Pattern-6
+--------------
+9 9 9 9 9 9 9 9 9 9 
+8 8 8 8 8 8 8 8 8 8 
+7 7 7 7 7 7 7 7 7 7 
+6 6 6 6 6 6 6 6 6 6 
+5 5 5 5 5 5 5 5 5 5 
+4 4 4 4 4 4 4 4 4 4 
+3 3 3 3 3 3 3 3 3 3 
+2 2 2 2 2 2 2 2 2 2 
+1 1 1 1 1 1 1 1 1 1 
+0 0 0 0 0 0 0 0 0 0
+	 */
+	public static void pattern6() {
+		System.out.println("Pattern-6");
+		System.out.println("--------------");
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.print((9 - i) + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	/*
+	 * Pattern-7
+--------------
+A B C D E F G H I J 
+A B C D E F G H I J 
+A B C D E F G H I J 
+A B C D E F G H I J 
+A B C D E F G H I J 
+A B C D E F G H I J 
+A B C D E F G H I J 
+A B C D E F G H I J 
+A B C D E F G H I J 
+A B C D E F G H I J
+	 */
+	public static void pattern7() {
+		System.out.println("Pattern-7");
+		System.out.println("--------------");
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.print((char) (65 + j) + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	/*
+	 * Pattern-8
+--------------
+A A A A A A A A A A 
+B B B B B B B B B B 
+C C C C C C C C C C 
+D D D D D D D D D D 
+E E E E E E E E E E 
+F F F F F F F F F F 
+G G G G G G G G G G 
+H H H H H H H H H H 
+I I I I I I I I I I 
+J J J J J J J J J J 
+	 */
+	public static void pattern8() {
+		System.out.println("Pattern-8");
+		System.out.println("--------------");
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.print((char) (65 + i) + " ");
+			}
+			System.out.println();
+		}
+	}
 }
