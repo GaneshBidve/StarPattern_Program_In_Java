a4a6db3248cded913453877a3d1251f6aa7b64da
Ganesh V Bidve <31819293+GaneshBidve@users.noreply.github.com>
2020-02-24 23:00:01 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -244,4 +244,29 @@ J I H G F E D C B A
 			System.out.println();
 		}
 	}
+	
+		/*
+	 * Pattern-10
+--------------
+J J J J J J J J J J 
+I I I I I I I I I I 
+H H H H H H H H H H 
+G G G G G G G G G G 
+F F F F F F F F F F 
+E E E E E E E E E E 
+D D D D D D D D D D 
+C C C C C C C C C C 
+B B B B B B B B B B 
+A A A A A A A A A A
+	 */
+	public static void pattern10() {
+		System.out.println("Pattern-10");
+		System.out.println("--------------");
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.print((char) (74 - i) + " ");
+			}
+			System.out.println();
+		}
+	}
 }
